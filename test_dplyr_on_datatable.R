e44398952b21b8b0613a3e421296b031d38db8ad
amchercashin <amchercashin@gmail.com>
2014-11-28 13:05:39 +0300

    test update

--- test_dplyr_on_datatable.R
+++ test_dplyr_on_datatable.R
@@ -11,9 +11,9 @@ df_2<-data.frame(k=sample(seq(count),count),five=rnorm(count),six=rnorm(count),s
 dt_1<-as.data.table(df_1)
 dt_2<-as.data.table(df_2)
 
-dt_native_syntax <- function(){ab<-merge(dt_1,dt_2, by="k")}
-dt_dplyr_syntax <- function() {ab<-inner_join(dt_1,dt_2, by="k")}
-df_dplyr_syntax <- function() {ab2<-inner_join(df_1,df_2, by="k")}
+dt_native_syntax <- function(){merge(dt_1,dt_2, by="k")}
+dt_dplyr_syntax <- function() {inner_join(dt_1,dt_2, by="k")}
+df_dplyr_syntax <- function() {inner_join(df_1,df_2, by="k")}
 
 print(microbenchmark(data_table_native_syntax=dt_native_syntax(),data_table_dplyr_syntax=dt_dplyr_syntax(),
-               data_frame_dplyr_syntax=df_dplyr_syntax(), times =20))
+               data_frame_dplyr_syntax=df_dplyr_syntax(), times =10))
